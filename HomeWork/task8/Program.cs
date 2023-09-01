b521e6210b7e90ed8f10d2d3f3d30fcfc1412d7d
Alan - Sarmat <alansarmatboraev@gmail.com>
2023-09-01 15:21:27 +0300

    task8 done

--- HomeWork/task8/Program.cs
+++ HomeWork/task8/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num = Convert.ToInt32(Console.ReadLine()!);
+for (int i = 1; i <= num; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($" {i}");
+    }
+}
