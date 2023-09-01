42e05365f5679de18221a7046af2b4335dae513c
Alan - Sarmat <alansarmatboraev@gmail.com>
2023-09-01 14:27:08 +0300

    task2 done

--- HomeWork/task2/Program.cs
+++ HomeWork/task2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numA = Convert.ToInt32(Console.ReadLine()!);
+int numB = Convert.ToInt32(Console.ReadLine()!);
+if (numA < numB)
+{
+    Console.Write($"min= {numA}, max= {numB}");
+}
+else if (numA > numB)
+{
+    Console.Write($"min= {numB}, max= {numA}");
+}
+else
+{
+    System.Console.WriteLine("числа равны");
+}
+
