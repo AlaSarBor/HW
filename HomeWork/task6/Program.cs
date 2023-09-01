be06096dbcebdd23cf62941e3a0ee844916b3104
Alan - Sarmat <alansarmatboraev@gmail.com>
2023-09-01 15:05:03 +0300

    task6 done

--- HomeWork/task6/Program.cs
+++ HomeWork/task6/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num = Convert.ToInt32(Console.ReadLine()!);
+if (num % 2 == 0)
+{
+    System.Console.WriteLine("yes");
+}
+else
+{
+    System.Console.WriteLine("no");
+}
