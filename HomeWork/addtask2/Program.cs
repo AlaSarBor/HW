a0222deff82f97afa98611a6910646fce81910d3
Alan - Sarmat <alansarmatboraev@gmail.com>
2023-09-01 16:13:09 +0300

    addtask2 done

---
+++ HomeWork/addtask2/Program.cs
@@ -0,0 +1,10 @@
+﻿int num = Convert.ToInt32(Console.ReadLine()!);
+string text = Convert.ToString(num);
+if (text.Length > 2)
+{
+    Console.WriteLine("третья цифра -> " + text[2]);
+}
+else
+{
+    Console.WriteLine("-");
+}
