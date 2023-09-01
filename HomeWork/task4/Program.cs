6d619440c6924fad21ba1ef65d48812cb4a82ba7
Alan - Sarmat <alansarmatboraev@gmail.com>
2023-09-01 14:59:32 +0300

    task4 done

--- HomeWork/task4/Program.cs
+++ HomeWork/task4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.DataAnnotations;
+
+int num1 = Convert.ToInt32(Console.ReadLine()!);
+int num2 = Convert.ToInt32(Console.ReadLine()!);
+int num3 = Convert.ToInt32(Console.ReadLine()!);
+int max = num1;
+if (max < num2)
+{
+    max = num2;
+}
+else if (max < num3)
+{
+    max = num3;
+}
+Console.WriteLine(max);
